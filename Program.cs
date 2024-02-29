7b81b13e57f464c4156ea8843f532d6d12924046
gabrielmitak <mitak.gabriel@zst.radom.pl>
2024-02-29 13:00:43 +0100

    files

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+       
+        string filePath = @"C:/test/test_gab_mit.txt";
+
+        try
+        {
+            
+            string fileContent = File.ReadAllText(filePath);
+
+          
+            Console.WriteLine("Zawartość pliku:");
+            Console.WriteLine(fileContent);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Nie można odnaleźć pliku.");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Wystąpił błąd podczas odczytu pliku.");
+        }
+
+        Console.ReadLine();
+    }
+}
